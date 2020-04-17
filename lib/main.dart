c2649b56a7f1db21efe8d5fc24e1796d2ba27109
kapil jhajhria <jhajhria44@gmail.com>
2020-04-17 23:25:06 +0530

    completed, only button shadows remaining

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: RegisterPage()));
+void main() => runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: "Poppins",
+      primaryColor: Color.fromRGBO(255, 122, 122, 1.0),
+      accentColor: Color.fromRGBO(56, 204, 140, 1.0),
+    ),
+    home: RegisterPage()));
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -28,9 +34,9 @@ class _RegisterPageState extends State<RegisterPage> {
                 border: OutlineInputBorder(),
                 hintText: fieldName,
                 contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 hintStyle:
-                    TextStyle(fontWeight: FontWeight.w800, fontSize: 18)),
+                    TextStyle(fontWeight: FontWeight.w600, fontSize: 18)),
           ),
         ),
         padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
@@ -41,7 +47,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HSLColor.fromAHSL(1.0, 0, 1.0, .74).toColor(),
+      backgroundColor: Color.fromRGBO(255, 122, 122, 1.0),
       body: SingleChildScrollView(
         controller: myScrollController,
         child: Column(
@@ -60,7 +66,6 @@ class _RegisterPageState extends State<RegisterPage> {
                   Expanded(
                       flex: 6,
                       child: Container(
-//                        color: Colors.amberAccent,
                         width: MediaQuery.of(context).size.width * 0.85,
                         child: Center(
                           child: Column(
@@ -71,15 +76,17 @@ class _RegisterPageState extends State<RegisterPage> {
                                 style: TextStyle(
                                     fontSize: 32,
                                     color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                                    fontWeight: FontWeight.w700),
                                 textAlign: TextAlign.center,
                               ),
                               Text(
                                 "See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.",
                                 style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white70,
-                                ),
+                                    wordSpacing: 4,
+                                    height: 2,
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600),
                                 textAlign: TextAlign.center,
                                 textScaleFactor: 1.0,
                               )
@@ -95,24 +102,36 @@ class _RegisterPageState extends State<RegisterPage> {
                             MediaQuery.of(context).size.height,
                             duration: Duration(milliseconds: 500),
                             curve: Curves.linear);
-                        setState(() {});
+//                        setState(() {});
                       },
                       child: Card(
-                        color: Colors.purple,
+                        color: Color.fromRGBO(96, 85, 165, 1.0),
                         margin: EdgeInsets.all(5),
                         elevation: 10.0,
 //                      width: MediaQuery.of(context).size.width * 0.85,
                         child: Container(
                           width: MediaQuery.of(context).size.width * 0.85,
                           child: Center(
-                            child: Text(
-                              "Try it free 7 days then \n \$20/mo. thereafter",
+                              child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
                               style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
+                                color: Colors.white,
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: 'Try it free 7 days ',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700)),
+                                TextSpan(
+                                    text: "then \n \$20/mo. thereafter",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400)),
+                              ],
                             ),
-                          ),
+                          )),
                         ),
                       ),
                     ),
@@ -144,16 +163,15 @@ class _RegisterPageState extends State<RegisterPage> {
                       GestureDetector(
                         onTap: () {
                           myScrollController.animateTo(
-                              MediaQuery.of(context).size.height,
+                              -MediaQuery.of(context).size.height,
                               duration: Duration(milliseconds: 500),
                               curve: Curves.linear);
                           setState(() {});
                         },
                         child: Card(
-                          color: Colors.green,
+                          color: Color.fromRGBO(56, 204, 140, 1.0),
                           margin: EdgeInsets.all(5),
                           elevation: 10.0,
-//                      width: MediaQuery.of(context).size.width * 0.85,
                           child: Container(
                             margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
                             height: MediaQuery.of(context).size.height * 0.1,
@@ -164,7 +182,8 @@ class _RegisterPageState extends State<RegisterPage> {
                                 style: TextStyle(
                                     fontSize: 19,
                                     color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                                    fontWeight: FontWeight.w600,
+                                    wordSpacing: 2),
                               ),
                             ),
                           ),
@@ -173,21 +192,37 @@ class _RegisterPageState extends State<RegisterPage> {
                       Expanded(
                         flex: 1,
                         child: Container(
-                          width: MediaQuery.of(context).size.width * 0.60,
-                          margin: EdgeInsets.only(top: 10),
-                          alignment: Alignment.topCenter,
-                          child: Text(
-                            "By clicking the button, you are agreeing to our Terms and Services",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
+                            width: MediaQuery.of(context).size.width * 0.60,
+                            margin: EdgeInsets.only(top: 10),
+                            alignment: Alignment.topCenter,
+                            child: RichText(
+                              textAlign: TextAlign.center,
+                              text: TextSpan(
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text:
+                                          'By clicking the button, you are agreeing to our  ',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Color.fromRGBO(
+                                              185, 182, 211, 1.0),
+                                          fontWeight: FontWeight.w400)),
+                                  TextSpan(
+                                      text: "Terms and Services",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.red,
+                                          fontWeight: FontWeight.w600)),
+                                ],
+                              ),
+                            )),
                       ),
                     ],
                   )),
-              color: Colors.green,
+              color: Color.fromRGBO(56, 204, 140, 1.0),
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
             ),
