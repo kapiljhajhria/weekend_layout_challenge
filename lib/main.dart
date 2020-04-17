8dcc07f2e4e29461261b7e49ffabebc1321ff8c5
kapil jhajhria <jhajhria44@gmail.com>
2020-04-17 21:53:09 +0530

    rough layout completed.
    fit & finish remaining

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,27 @@ class _RegisterPageState extends State<RegisterPage> {
     super.initState();
   }
 
+  Widget formInputTextBox(String fieldName) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        child: Center(
+          child: TextField(
+            style: TextStyle(fontSize: 26),
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: fieldName,
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                hintStyle:
+                    TextStyle(fontWeight: FontWeight.w800, fontSize: 18)),
+          ),
+        ),
+        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -100,7 +121,72 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
             ),
             Container(
-              child: Text("Hello"),
+              padding: EdgeInsets.fromLTRB(
+                  0,
+                  MediaQuery.of(context).size.height * .04,
+                  0,
+                  MediaQuery.of(context).size.height * .1),
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  margin: EdgeInsets.fromLTRB(
+                      MediaQuery.of(context).size.width * .08,
+                      MediaQuery.of(context).size.height * .04,
+                      MediaQuery.of(context).size.width * .08,
+                      0),
+                  child: Column(
+                    children: <Widget>[
+                      formInputTextBox("First Name"),
+                      formInputTextBox("Last Name"),
+                      formInputTextBox("Email Address"),
+                      formInputTextBox("Password"),
+                      GestureDetector(
+                        onTap: () {
+                          myScrollController.animateTo(
+                              MediaQuery.of(context).size.height,
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.linear);
+                          setState(() {});
+                        },
+                        child: Card(
+                          color: Colors.green,
+                          margin: EdgeInsets.all(5),
+                          elevation: 10.0,
+//                      width: MediaQuery.of(context).size.width * 0.85,
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            height: MediaQuery.of(context).size.height * 0.1,
+                            width: MediaQuery.of(context).size.width * 0.65,
+                            child: Center(
+                              child: Text(
+                                "CLAIM YOUR FREE TRIAL",
+                                style: TextStyle(
+                                    fontSize: 19,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.60,
+                          margin: EdgeInsets.only(top: 10),
+                          alignment: Alignment.topCenter,
+                          child: Text(
+                            "By clicking the button, you are agreeing to our Terms and Services",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
               color: Colors.green,
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
